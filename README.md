514a402b7e1cefb44d1c1a2045895026aac8035a
sulicat <suleiman.barakat@mavs.uta.edu>
2018-06-10 20:24:25 -0500

    first qt window

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # graphics
 A repo to use while I learn and mess around in the realm of computer graphics.
+
+
+## Dependancies
+QT
---
+++ basic_qt/qt_basic.cpp
@@ -0,0 +1,13 @@
+#include <QApplication>
+#include <QPushButton>
+
+int main(int argc, char **argv){
+
+	QApplication app (argc, argv);
+	QPushButton button;
+	button.setText("My text");
+	button.setToolTip("A tooltip");
+	button.show();
+
+	return app.exec();
+}
